096aaf4de52a6ae4c56bc4dab9e0d7655a55cfa4
mbett <mbett@cs.cmu.edu>
2022-08-12 11:20:29 -0400

    Update firebase-connector.js

--- run/firebase-connector.js
+++ run/firebase-connector.js
@@ -2,6 +2,13 @@
   // Import the functions you need from the SDKs you need
   import { initializeApp } from "https://www.gstatic.com/firebasejs/9.9.2/firebase-app.js";
   import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.9.2/firebase-analytics.js";
+
+// Import functions to get the database root, set a location and update the values
+//
+import { getDatabase, ref, set, update, child, get, onValue, remove } from "https://www.gstatic.com/firebasejs/9.9.2/firebase-database.js";
+
+
+
   // TODO: Add SDKs for Firebase products that you want to use
   // https://firebase.google.com/docs/web/setup#available-libraries
 
