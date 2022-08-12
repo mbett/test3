7f774ce512c36b4fca5afd2bc672379367c0a3d8
mbett <mbett@cs.cmu.edu>
2022-08-12 09:44:26 -0400

    Create firebase-connector.js

---
+++ firebase-connector.js
@@ -0,0 +1,23 @@
+// <script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.9.2/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.9.2/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyCmprwZYUG5w0wB8Pa3AeGxeCxQp40m3LQ",
+    authDomain: "test3-24a5a.firebaseapp.com",
+    projectId: "test3-24a5a",
+    storageBucket: "test3-24a5a.appspot.com",
+    messagingSenderId: "333990236385",
+    appId: "1:333990236385:web:53a034772bf17a95fb67a0",
+    measurementId: "G-XPSE5H2EHE"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+// </script>
