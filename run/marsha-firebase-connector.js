e50664f8ec5842e4c386b187d0a95480fa2c0c61
mbett <mbett@cs.cmu.edu>
2022-08-23 14:39:41 -0400

    Create marsha-firebase-connector.js

---
+++ run/marsha-firebase-connector.js
@@ -0,0 +1,36 @@
+// <script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.9.2/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.9.2/firebase-analytics.js";
+
+
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig-michael = {
+    apiKey: "AIzaSyCmprwZYUG5w0wB8Pa3AeGxeCxQp40m3LQ",
+    authDomain: "test3-24a5a.firebaseapp.com",
+    projectId: "test3-24a5a",
+    storageBucket: "test3-24a5a.appspot.com",
+    messagingSenderId: "333990236385",
+    appId: "1:333990236385:web:53a034772bf17a95fb67a0",
+    measurementId: "G-XPSE5H2EHE"
+  };
+
+ const firebaseConfig = {
+    apiKey: "AIzaSyDe0dsXJkiCPZsNihE3p_5sU7Mc9PCvYsQ",
+    authDomain: "learningdatabase-f4bb0.firebaseapp.com",
+    projectId: "learningdatabase-f4bb0",
+    storageBucket: "learningdatabase-f4bb0.appspot.com",
+    messagingSenderId: "266285362578",
+    appId: "1:266285362578:web:54c32e75e84a064222654b",
+    measurementId: "G-60LK8HT0L2"
+  };
+
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+// </script>
